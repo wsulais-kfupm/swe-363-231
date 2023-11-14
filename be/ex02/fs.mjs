d3febaf2278b74e8f6b22cc6d88b3a4660f014fa
wsulais-kfupm <s202032440@kfupm.edu.sa>
2023-11-14 22:51:21 +0300

    fix(be/ex02): Copy over the files in part 2

--- be/ex02/fs.mjs
+++ be/ex02/fs.mjs
@@ -1,4 +1,5 @@
-import { readdir } from "node:fs/promises";
+import { readdir, copyFile } from "node:fs/promises";
+import process from "https://deno.land/std@0.120.0/node/process.ts";
 
 async function get_dir(path, ext) {
 	const exts = ext ?? ["txt", "jpg"];
@@ -7,6 +8,19 @@ async function get_dir(path, ext) {
 }
 
 const args = process.argv.slice();
-const files = await get_dir(args[2], args.slice(3));
+const src_dir = args[2];
+const dest_dir = args[3] ?? ".";
+const filter = args.slice(4);
+
+console.log(`Copying all ${filter ?? "default"} files in '${src_dir}' to '${dest_dir}' `)
+
+const files = await get_dir(src_dir, filter);
 
 console.log(files);
+
+for (const file of files) {
+	const src = src_dir + "/" + file;
+	const dest = dest_dir + "/" + file;
+	console.log(`Copying '${src}' to '${dest}'`);
+	await copyFile(src, dest);
+}
